01d9edb49b75ced9ed6cef7b7ebeb019bceb04b9
pepel <pepel@MSI>
2025-09-25 15:25:57 +0200

    Primera modificación

--- src/Prueba/prueba.java
+++ src/Prueba/prueba.java
@@ -3,6 +3,10 @@ package Prueba;
 public class prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		int a = 2;
+		int b = 4;
+		int suma = a + b;
+		
+		System.out.println("El resultado de la suma es: " + suma);
 	}
 }
