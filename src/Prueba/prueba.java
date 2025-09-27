9c8e39619efc223c984f9022b16a4f2b82f90a3a
pepel <pepel@MSI>
2025-09-27 14:59:54 +0200

    Octava Modificación

--- src/Prueba/prueba.java
+++ src/Prueba/prueba.java
@@ -7,7 +7,11 @@ public class prueba {
 		int b = 4;
 		int c = 5;
 		int suma = a + b + c;
+		int mul1 = a * c;
+		int mul2 = b * c;
 		
 		System.out.println("El resultado de la suma es: " + suma);
+		System.out.println("El resultado de la primera multiplicación es: " + mul1);
+		System.out.println("El resultado de la segunda multiplicación es: " + mul2);
 	}
 }
