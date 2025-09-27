b5d7961d1861a74ae936f8e1b8d701559f930b02
pepel <pepel@MSI>
2025-09-27 14:28:14 +0200

    Septima Modificación

--- src/Prueba/prueba.java
+++ src/Prueba/prueba.java
@@ -5,8 +5,9 @@ public class prueba {
 	public static void main(String[] args) {
 		int a = 2;
 		int b = 4;
-		int suma = a + b ;
-		int mul = a * b;
+		int c = 5;
+		int suma = a + b;
+		int mul = suma * c;
 		
 		System.out.println("El resultado de la suma es: " + suma);
 		System.out.println("El resultado de la multiciplación es: " + mul);	
