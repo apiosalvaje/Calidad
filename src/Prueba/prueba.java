f54ab138b4d0001d939ceefa1c70b73f9dc4b93b
pepel <pepel@MSI>
2025-09-27 15:05:39 +0200

    Última modificación

--- src/Prueba/prueba.java
+++ src/Prueba/prueba.java
@@ -3,15 +3,7 @@ package Prueba;
 public class prueba {
 
 	public static void main(String[] args) {
-		int a = 2;
-		int b = 4;
-		int c = 5;
-		int suma = a + b + c;
-		int mul1 = a * c;
-		int mul2 = b * c;
-		
-		System.out.println("El resultado de la suma es: " + suma);
-		System.out.println("El resultado de la primera multiplicación es: " + mul1);
-		System.out.println("El resultado de la segunda multiplicación es: " + mul2);
+
+		System.out.println("Rama no continuada");
 	}
 }
