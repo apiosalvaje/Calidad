79063c1ae7054bcd4a4984ef4cbfd1d0ac6440b5
pepel <pepel@MSI>
2025-09-27 14:10:43 +0200

    Quinta modificación

--- src/Prueba/prueba.java
+++ src/Prueba/prueba.java
@@ -5,7 +5,8 @@ public class prueba {
 	public static void main(String[] args) {
 		int a = 2;
 		int b = 4;
-		int suma = a + b;
+		int c = 5;
+		int suma = a + b + c;
 		
 		System.out.println("El resultado de la suma es: " + suma);
 	}
